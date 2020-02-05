67c49128333e7121d7aaecdf30abfe0e9be6a5c2
dixantrai <56809131+dixantrai@users.noreply.github.com>
2020-02-05 14:41:54 -0500

    Update sync.py
    Update sync.py to use CREATEDON date field when MODIFIEDON is not available.

--- tap_dynamics/sync.py
+++ tap_dynamics/sync.py
@@ -9,9 +9,22 @@ from tap_dynamics.discover import discover, get_optionset_metadata, get_optionse
 LOGGER = singer.get_logger()
 
 MODIFIED_DATE_FIELD = 'modifiedon'
+CREATEDON_DATE_FIELD = 'createdon'
 
 def get_bookmark(state, stream_name, default):
-    return state.get('bookmarks', {}).get(stream_name, default)
+    LOGGER.info('')
+    LOGGER.info('')
+    LOGGER.info('*** Entered get_bookmark ***')
+    LOGGER.info('State: "{}"'.format(state))
+    LOGGER.info('Stream_Name: "{}", Default: "{}", MODIFIED_DATE_FIELD: "{}"'.format( stream_name, default, MODIFIED_DATE_FIELD))
+
+    output = state.get('bookmarks', {}).get(stream_name, default)
+
+    LOGGER.info('state.get() output: "{}"'.format(output))
+    LOGGER.info('*** Leaving get_bookmark and returning above output ***')
+    LOGGER.info('')
+
+    return output
 
 def write_bookmark(state, stream_name, value):
     if 'bookmarks' not in state:
@@ -35,15 +48,23 @@ def sync_stream(service, catalog, state, start_date, stream, mdata):
     entitycls = service.entities[stream_name]
     query = service.query(entitycls)
 
-    if hasattr(entitycls, MODIFIED_DATE_FIELD):
-        LOGGER.info('{} - Syncing data since {}'.format(stream.tap_stream_id, last_datetime))
+    if hasattr(entitycls, MODIFIED_DATE_FIELD) or hasattr(entitycls, CREATEDON_DATE_FIELD):
+        if hasattr(entitycls, MODIFIED_DATE_FIELD):
+            query_field = MODIFIED_DATE_FIELD
+        else:
+            query_field = CREATEDON_DATE_FIELD # Added to handle the append-only "Audits" table
+        LOGGER.info('')
+        LOGGER.info('{} - Syncing data since {} using replication_key {}'.format(stream.tap_stream_id, last_datetime, query_field))
+        LOGGER.info('')
         query = (
             query
-            .filter(getattr(entitycls, MODIFIED_DATE_FIELD) >= singer.utils.strptime_with_tz(last_datetime))
-            .order_by(getattr(entitycls, MODIFIED_DATE_FIELD).asc())
+            .filter(getattr(entitycls, query_field) >= singer.utils.strptime_with_tz(last_datetime))
+            .order_by(getattr(entitycls, query_field).asc())
         )
     else:
+        LOGGER.info('')
         LOGGER.info('{} - Syncing using full replication'.format(stream.tap_stream_id))
+        LOGGER.info('')
 
     schema = stream.schema.to_dict()
     optionset_map = get_optionset_metadata(service, stream.tap_stream_id)
@@ -60,9 +81,14 @@ def sync_stream(service, catalog, state, start_date, stream, mdata):
                 if prop_name in optionset_map:
                     label_prop_name = get_optionset_fieldname(prop_name)
                     if value is None:
-                        dict_record[label_prop_name] = None
+                        #dict_record[label_prop_name] = None
+                        # the above turns the Python null of None to the string "None", which is not good for the target DB
+                        dict_record[label_prop_name] = ''
                     else:
-                        dict_record[label_prop_name] = optionset_map[prop_name][value]
+                        optionset_value = optionset_map[prop_name].get(value)
+                        if optionset_value is None:
+                            LOGGER.warn('Label not found for value `{}` on `{}`'.format(value, prop_name))
+                        dict_record[label_prop_name] = optionset_value
 
             if MODIFIED_DATE_FIELD in dict_record and dict_record[MODIFIED_DATE_FIELD] > max_modified:
                     max_modified = dict_record[MODIFIED_DATE_FIELD]
